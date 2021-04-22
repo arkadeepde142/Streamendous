f9ebf960c6160da4dbc8332b2377b18ccf670c67
Arkadeep De <arkadeepde142@gmail.com>
2021-04-22 17:46:32 +0530

    Mongo setup done

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,37 @@
-import express from 'express';
-import {json} from 'body-parser';
+import express from "express";
+import { json } from "body-parser";
+import mongoose, { connect } from "mongoose";
+import dotenv from 'dotenv';
 
-const app = express();
+const main = async () => {
+  const app = express();
 
-app.use(json());
 
-app.listen(5000, () => {
-    console.log('server running');
-})
+  app.use(json());
+
+  dotenv.config();
+    
+
+  try {
+    await mongoose.connect(process.env.URI as string, {
+      useCreateIndex: true,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+  } catch (e) {
+    console.error(e);
+  }
+
+  const server = app.listen(5000, () => {
+    console.log("server running");
+  });
+
+process.on("SIGTERM", async () => {
+        server.close((err)=> {
+            console.log(err);
+        })
+        await mongoose.connection.close(true);
+});
+};
+
+main();
